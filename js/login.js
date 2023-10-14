3b2e03516b73fd8f9febde7969883afdd935947a
Chua Xinhui <xinhui.chua188@gmail.com>
2023-10-14 16:49:41 +0800

    Added success and fail alertbox for login

--- js/login.js
+++ js/login.js
@@ -2,13 +2,13 @@
 var firebaseConfig = {
   // put your api stuff here
   apiKey: "AIzaSyCT4ilVsx9OHQwazEKXgPyHaV1wus6e_Ik",
-authDomain: "test1-69744.firebaseapp.com",
-databaseURL: "https://test1-69744-default-rtdb.asia-southeast1.firebasedatabase.app",
-projectId: "test1-69744",
-storageBucket: "test1-69744.appspot.com",
-messagingSenderId: "587694443578",
-appId: "1:587694443578:web:f0123996bd6f63ed8ef903",
-measurementId: "G-0LBGW6RB64"
+  authDomain: "test1-69744.firebaseapp.com",
+  databaseURL: "https://test1-69744-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test1-69744",
+  storageBucket: "test1-69744.appspot.com",
+  messagingSenderId: "587694443578",
+  appId: "1:587694443578:web:f0123996bd6f63ed8ef903",
+  measurementId: "G-0LBGW6RB64"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,10 +18,11 @@ const database = firebase.database()
 
 
 // Set up our login function
-function login () {
+function login() {
   // Get all our input fields
   email = document.getElementById('email').value
   password = document.getElementById('password').value
+  var loginBtn = document.getElementById('loginBtn');
 
   // Validate input fields
   if (validate_email(email) == false || validate_password(password) == false) {
@@ -31,36 +32,70 @@ function login () {
   }
 
   auth.signInWithEmailAndPassword(email, password)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser
 
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
 
-    // Create User data
-    var user_data = {
-      last_login : Date.now()
-    }
+      // Create User data
+      var user_data = {
+        last_login: Date.now()
+      }
 
-    // Push to Firebase Database
-    database_ref.child('users/' + user.uid).update(user_data)
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
 
-    // DOne
-    window.location.href = "main-page.html"
-    alert("user signed in")
 
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
+    
+      // //alert box
+      // Display a Bootstrap success alert
+      var successAlert = document.getElementById('success-alert');
+      successAlert.style.display = 'block';
 
-    console.log(error_code)
-    console.log(error_message)
-    alert(error_message);
-  })
+      setInterval(function(){
+        window.location.href = "main-page.html",4000
+      })
+
+      
+     
+
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+
+      console.log(error_code)
+      console.log(error_message)
+      alert(error_message);
+    })
 }
+
+
+//successbox
+function successBox(){
+  document.getElementById("alertBox").innerHTML= '<div class="alert alert-success" role="alert"> This is a success alert—check it out!</div>';
+
+
+}
+// Function to show a Bootstrap alert with a custom message
+function showAlert(message, alertType) {
+  const alertContainer = document.getElementById('alertBox');
+  const alertHTML = `
+    <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+      ${message}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  `;
+
+  alertContainer.innerHTML = alertHTML;
+}
+
+
 
 
 
