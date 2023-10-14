c26efeca9b4384b39c11b69f3d8ba21df8f06a23
xyscis <xinyan.chua.2022@scis.smu.edu.sg>
2023-10-14 12:40:00 +0800

    modified v2 , and prepare to merge to main

--- js/timer.js
+++ js/timer.js
@@ -1,55 +1,70 @@
+//timer
 let timer;
 let countdownTime = 1500; // 25 minutes in seconds
 let isPaused = false; // To track whether the timer is paused
 
 const display = document.getElementById("timer-display");
-const startButton = document.getElementById("start-button");
-const pauseButton = document.getElementById("pause-button"); // Add a "Pause" button
+
+const startplayresumeButton = document.getElementById(
+  "start-play-resume-button"
+);
+const icon = document.getElementById("icon");
 const resetButton = document.getElementById("reset-button");
+const addMinutesButton = document.getElementById("add-minutes-button");
+const minusMinutesButton = document.getElementById("minus-minutes-button");
 
-function updateDisplay(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    display.textContent = formattedTime;
-}
 
-function startTimer() {
-    timer = setInterval(function() {
-        if (!isPaused) { // Only decrement the time if not paused
-            countdownTime--;
-            updateDisplay(countdownTime);
+function toggleTimer() {
+    if (timer == null) {
+      // timer has no value when start timer for the first time
+      timer = setInterval(function () {
+        if (!isPaused) {
+          // Only decrement the time if not paused
+          countdownTime--;
+          updateDisplay(countdownTime);
         }
         if (countdownTime === 0) {
-            clearInterval(timer);
-            alert("Time's up!");
+          clearInterval(timer);
+          alert("Time's up!");
         }
-    }, 1000);
-    startButton.disabled = true;
-    pauseButton.disabled = false; // Enable the "Pause" button when starting
-}
-
-function pauseTimer() {
-    isPaused = !isPaused; // Toggle the pause state
-    if (isPaused) {
-        pauseButton.textContent = "Resume"; // Change button text to "Resume" when paused
+      }, 1000);
+  
+      icon.src = "../img/pause.png";
     } else {
-        pauseButton.textContent = "Pause"; // Change button text back to "Pause" when resumed
+      isPaused = !isPaused;
+      if (isPaused) {
+        icon.src = "../img/play.png";
+      } else {
+        icon.src = "../img/pause.png";
+      }
     }
-}
-
-function resetTimer() {
+  }
+  
+  function resetTimer() {
     clearInterval(timer);
+    timer = null; // returns no value so can click play after reset
     countdownTime = 1500;
     updateDisplay(countdownTime);
     isPaused = false; // Reset the pause state
-    pauseButton.textContent = "Pause"; // Reset the "Pause" button text
-    startButton.disabled = false;
-    pauseButton.disabled = true; // Disable the "Pause" button when resetting
-}
-
-startButton.addEventListener("click", startTimer);
-pauseButton.addEventListener("click", pauseTimer); // Add event listener for the "Pause" button
-resetButton.addEventListener("click", resetTimer);
-
-
+    icon.src = "../img/play.png";
+  }
+  
+  function addMinutes() {
+    if (!isPaused || isPaused) {
+      countdownTime += 60; // Add 60 seconds (1 minute) to the countdown time
+      updateDisplay(countdownTime);
+    }
+  }
+  
+  function minusMinutes() {
+    if (!isPaused || isPaused) {
+      countdownTime -= 60; // minus 60 seconds (1 minute) to the countdown time
+      updateDisplay(countdownTime);
+    }
+  }
+  
+  startplayresumeButton.addEventListener("click", toggleTimer);
+  resetButton.addEventListener("click", resetTimer);
+  addMinutesButton.addEventListener("click", addMinutes);
+  minusMinutesButton.addEventListener("click", minusMinutes);
+  
