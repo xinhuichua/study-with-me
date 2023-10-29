9d31a45c0794a551c94ed95dd2aaf31ecd9bd7bb
Chua Xinhui <xinhui.chua188@gmail.com>
2023-10-29 14:42:10 +0800

    added quotes API to profile page

---
+++ js/quotes.js
@@ -0,0 +1,16 @@
+var category = 'success'
+const API_KEY = '+HnQMrGNtc7dnxh61ujwVQ==DPYFTuSfVmMK4YUF'
+
+axios.get('https://api.api-ninjas.com/v1/quotes', {
+    params: { category: category },
+    headers: { 'X-Api-Key': API_KEY }
+})
+    .then(response => {
+        console.log(response.data);
+        var data = response.data[0].quote
+         document.getElementById('quote').innerText = data 
+        
+    })
+    .catch(error => {
+        console.error('Error: ', error.response.data);
+    });
