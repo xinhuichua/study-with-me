7ccfd41582c2378d2f7fcdeabb608922b3e26836
xyscis <xinyan.chua.2022@scis.smu.edu.sg>
2023-10-29 13:36:29 +0800

    modified timer function

--- js/timer.js
+++ js/timer.js
@@ -2,7 +2,7 @@
 let timer;
 let countdownTime = 1500; // 25 minutes in seconds
 let isPaused = false; // To track whether the timer is paused
-
+let additionalTime = 0;
 const display = document.getElementById("timer-display");
 
 const startplayresumeButton = document.getElementById(
@@ -20,6 +20,15 @@ function updateDisplay(time) {
     .toString()
     .padStart(2, "0")}`;
   display.textContent = formattedTime;
+ 
+  if (time === 0) {  // Disable the decrease button when the timer hits 0
+    minusMinutesButton.disabled = true;
+    startplayresumeButton.disabled =true
+
+  } else {
+    minusMinutesButton.disabled = false;
+    startplayresumeButton.disabled =false;
+  }
 }
 
 function toggleTimer() {
@@ -29,12 +38,15 @@ function toggleTimer() {
         if (!isPaused) {
           // Only decrement the time if not paused
           countdownTime--;
+          if (countdownTime === 0) {
+            handleTimerZero()
+          
+            clearInterval(timer);
+  
+          }
           updateDisplay(countdownTime);
         }
-        if (countdownTime === 0) {
-          clearInterval(timer);
-          alert("Time's up!");
-        }
+        
       }, 1000);
   
       icon.src = "../img/pause.png";
@@ -42,11 +54,15 @@ function toggleTimer() {
       isPaused = !isPaused;
       if (isPaused) {
         icon.src = "../img/play.png";
+        
       } else {
         icon.src = "../img/pause.png";
+      
       }
     }
   }
+
+
   
   function resetTimer() {
     clearInterval(timer);
@@ -58,21 +74,48 @@ function toggleTimer() {
   }
   
   function addMinutes() {
-    if (!isPaused || isPaused) {
-      countdownTime += 60; // Add 60 seconds (1 minute) to the countdown time
+    if (countdownTime === 0) {
+      updateDisplay(countdownTime);
+      minusMinutesButton.disabled = false; // Enable decrease button
+      startplayresumeButton.disabled = false; // Enable Start Play Resume button
+      countdownTime += 300; // Add 300 seconds (5 minutes) for example, you can change this value
+    } else if (!isPaused || isPaused) {
+      countdownTime += 300; // Add 300 seconds (5 minutes) to the countdown time
       updateDisplay(countdownTime);
     }
+    updateDisplay(countdownTime);
   }
   
   function minusMinutes() {
     if (!isPaused || isPaused) {
-      countdownTime -= 60; // minus 60 seconds (1 minute) to the countdown time
+      if (countdownTime > 0) {
+      countdownTime -= 300; // minus 300 seconds (1 minute) to the countdown time
+      minusMinutesButton.disabled = false;
       updateDisplay(countdownTime);
     }
+    else{
+      minusMinutesButton.disabled = true;
+      updateDisplay(countdownTime);
+
+    }
+    }
+  }
+  
+  function handleTimerZero() {
+    icon.src = "../img/play.png";
+    const successBody = document.querySelector('#successModal .modal-body');
+    successBody.innerText = "Time's up!";
+  
+    const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+    successModal.show();
+    
+    clearInterval(timer);
+    resetTimer()
   }
   
   startplayresumeButton.addEventListener("click", toggleTimer);
   resetButton.addEventListener("click", resetTimer);
   addMinutesButton.addEventListener("click", addMinutes);
   minusMinutesButton.addEventListener("click", minusMinutes);
+  
   
